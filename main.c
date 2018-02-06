14a12339b2ddf0e74213f1d20d08152cd90fb3ea
Joshua M. Clulow <jmc@joyent.com>
2018-02-06 09:20:29 +0000

    improve balancer UDS handling, forward TCP connections

--- main.c
+++ main.c
@@ -322,6 +322,16 @@ close_be:
 }
 
 static void
+bbal_uds_end(cconn_t *ccn, int event)
+{
+	backend_t *be = cconn_data(ccn);
+
+	fprintf(stdout, "[BE %d] ended!\n", be->be_id);
+	be->be_ok = B_FALSE;
+	cconn_fin(ccn);
+}
+
+static void
 bbal_uds_error(cconn_t *ccn, int event)
 {
 	backend_t *be = cconn_data(ccn);
@@ -346,11 +356,11 @@ bbal_uds_close(cconn_t *ccn, int event)
 }
 
 static int
-bbal_connect_uds(backend_t *be)
+bbal_connect_uds_common(backend_t *be, int *sockp)
 {
 	int e;
+	int sock = -1;
 	struct sockaddr_un sun;
-	int sock;
 
 	if ((sock = socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC,
 	    0)) < 0) {
@@ -370,6 +380,57 @@ bbal_connect_uds(backend_t *be)
 		goto fail;
 	}
 
+	*sockp = sock;
+	return (0);
+
+fail:
+	if (sock != -1) {
+		VERIFY0(close(sock));
+	}
+	errno = e;
+	return (-1);
+}
+
+/*
+ * Make a new connection to this backend for use in proxying a TCP connection.
+ */
+static int
+bbal_connect_uds_tcp(backend_t *be, cconn_t **ccnp)
+{
+	cconn_t *ccn;
+
+	if (cconn_alloc(&ccn) != 0) {
+		warn("cconn_alloc");
+		return (-1);
+	}
+	cconn_byteorder_set(ccn, CBUF_ORDER_LITTLE_ENDIAN);
+
+	int sock;
+	if (bbal_connect_uds_common(be, &sock) != 0) {
+		cconn_destroy(ccn);
+		return (-1);
+	}
+
+	cconn_attach(be->be_loop, ccn, sock);
+
+	*ccnp = ccn;
+	return (0);
+}
+
+/*
+ * Establish the standing connection to this backend that we use to forward
+ * UDP packets to and from remote peers.
+ */
+static int
+bbal_connect_uds(backend_t *be)
+{
+	int e;
+	int sock = -1;
+
+	if (bbal_connect_uds_common(be, &sock) != 0) {
+		return (-1);
+	}
+
 	cconn_destroy(be->be_conn);
 	if (cconn_alloc(&be->be_conn) != 0) {
 		e = errno;
@@ -380,6 +441,7 @@ bbal_connect_uds(backend_t *be)
 	cconn_data_set(be->be_conn, be);
 
 	cconn_on(be->be_conn, CCONN_CB_CONNECTED, bbal_uds_connected);
+	cconn_on(be->be_conn, CCONN_CB_END, bbal_uds_end);
 	cconn_on(be->be_conn, CCONN_CB_ERROR, bbal_uds_error);
 	cconn_on(be->be_conn, CCONN_CB_CLOSE, bbal_uds_close);
 	cconn_on(be->be_conn, CCONN_CB_DATA_AVAILABLE, bbal_uds_data);
@@ -558,6 +620,214 @@ bail:
 	cloop_ent_want(ent, CLOOP_CB_READ);
 }
 
+typedef struct {
+	cconn_t *prx_front;
+	cconn_t *prx_back;
+	boolean_t prx_flowing;
+} proxy_t;
+
+static void
+bbal_tcp_teardown(proxy_t *prx)
+{
+	if (prx->prx_front != NULL) {
+		cconn_abort(prx->prx_front);
+	}
+	if (prx->prx_back != NULL) {
+		cconn_abort(prx->prx_back);
+	}
+}
+
+static void
+bbal_tcp_front_data(cconn_t *ccn, int event)
+{
+	proxy_t *prx = cconn_data(ccn);
+
+	fprintf(stdout, "\tTCP FRONT DATA\n");
+
+	if (!prx->prx_flowing) {
+		/*
+		 * Still waiting for the backend connection to be established.
+		 */
+		fprintf(stdout, "\t\tnot yet flowing\n");
+		return;
+	}
+
+	/*
+	 * Push all of the data we have into the backend.
+	 */
+	cbufq_t *q = cconn_recvq(prx->prx_front);
+	cbuf_t *b;
+	while ((b = cbufq_deq(q)) != NULL) {
+		fprintf(stdout, "\t\tforward %d bytes\n", cbuf_available(b));
+		cbuf_resume(b);
+		if (cconn_send(prx->prx_back, b) != 0) {
+			cbuf_free(b);
+			warn("cconn_send to backend");
+			bbal_tcp_teardown(prx);
+			return;
+		}
+	}
+
+	cconn_more_data(prx->prx_front);
+}
+
+static void
+bbal_tcp_back_connect(cconn_t *ccn, int event)
+{
+	proxy_t *prx = cconn_data(ccn);
+
+	fprintf(stdout, "\tTCP BACK CONNECT\n");
+
+	/*
+	 * Connected to backend.  First send our preamble frame which identifies
+	 * the remote peer to the backend, then start the flow of data.
+	 */
+	cbuf_t *buf;
+	if (cbuf_alloc(&buf, 2048) != 0) {
+		err(1, "cbuf_alloc backend connect");
+	}
+	cbuf_byteorder_set(buf, CBUF_ORDER_LITTLE_ENDIAN);
+
+	const struct sockaddr_in *sin = cconn_sockaddr_in(prx->prx_front);
+	VERIFY0(cbuf_put_u32(buf, 3)); /* FRAME TYPE */
+	VERIFY0(cbuf_put_u32(buf, ntohl(sin->sin_addr.s_addr))); /* IP */
+	VERIFY0(cbuf_put_u32(buf, ntohs(sin->sin_port))); /* PORT */
+
+	if (cconn_send(ccn, buf) != 0) {
+		err(1, "cconn_send backend hello frame");
+	}
+
+	prx->prx_flowing = B_TRUE;
+	bbal_tcp_front_data(prx->prx_front, CCONN_CB_DATA_AVAILABLE);
+}
+
+static void
+bbal_tcp_back_data(cconn_t *ccn, int event)
+{
+	proxy_t *prx = cconn_data(ccn);
+
+	fprintf(stdout, "\tTCP BACK DATA\n");
+
+	/*
+	 * Push all of the data we have into the frontend.
+	 */
+	cbufq_t *q = cconn_recvq(prx->prx_back);
+	cbuf_t *b;
+	while ((b = cbufq_deq(q)) != NULL) {
+		fprintf(stdout, "\t\tforward %d bytes\n", cbuf_available(b));
+		cbuf_resume(b);
+		if (cconn_send(prx->prx_front, b) != 0) {
+			cbuf_free(b);
+			warn("cconn_send to frontend");
+			bbal_tcp_teardown(prx);
+			return;
+		}
+	}
+
+	cconn_more_data(prx->prx_back);
+}
+
+static void
+bbal_tcp_front_end(cconn_t *ccn, int event)
+{
+	proxy_t *prx = cconn_data(ccn);
+
+	fprintf(stdout, "\tTCP FRONT END\n");
+
+	if (cconn_fin(prx->prx_back) != 0) {
+		warn("cconn_fin");
+	}
+}
+
+static void
+bbal_tcp_back_end(cconn_t *ccn, int event)
+{
+	proxy_t *prx = cconn_data(ccn);
+
+	fprintf(stdout, "\tTCP BACK END\n");
+
+	if (cconn_fin(prx->prx_front) != 0) {
+		warn("cconn_fin");
+	}
+}
+
+static void
+bbal_tcp_error(cconn_t *ccn, int event)
+{
+	proxy_t *prx = cconn_data(ccn);
+
+	fprintf(stdout, "\tTCP ERROR (%s)\n", ccn == prx->prx_front ? "front" :
+	    "back");
+
+	bbal_tcp_teardown(prx);
+}
+
+static void
+bbal_tcp_incoming(cserver_t *cserver, int event)
+{
+	VERIFY3S(event, ==, CSERVER_CB_INCOMING);
+
+	proxy_t *prx;
+	if ((prx = calloc(1, sizeof (*prx))) == NULL) {
+		/*
+		 * XXX Exit now.  We could instead wait for more memory, but
+		 * we'd need to remember to call "cserver_accept()" later or
+		 * this incoming callback won't rearm.
+		 */
+		err(1, "incoming tcp calloc");
+	}
+
+	if (cserver_accept(cserver, &prx->prx_front) != 0) {
+		if (errno != EAGAIN) {
+			warn("cserver_accept");
+		}
+		return;
+	}
+	fprintf(stdout, "\tTCP inbound from %s\n",
+	    cconn_remote_addr_str(prx->prx_front));
+
+	/*
+	 * Select a backend for the remote peer.
+	 */
+	remote_t *rem = remote_lookup(
+	    &cconn_sockaddr_in(prx->prx_front)->sin_addr);
+	if (rem == NULL) {
+		warn("remote_lookup");
+		goto fail;
+	}
+	backend_t *be = backend_lookup(rem->rem_backend);
+	if (be == NULL) {
+		warn("backend_lookup");
+		goto fail;
+	}
+
+	fprintf(stdout, "\t\tbackend -> %d\n", be->be_id);
+
+	/*
+	 * Begin connecting to the backend.
+	 */
+	if (bbal_connect_uds_tcp(be, &prx->prx_back) != 0) {
+		goto fail;
+	}
+
+	cconn_data_set(prx->prx_front, prx);
+	cconn_on(prx->prx_front, CCONN_CB_DATA_AVAILABLE, bbal_tcp_front_data);
+	cconn_on(prx->prx_front, CCONN_CB_END, bbal_tcp_front_end);
+	cconn_on(prx->prx_front, CCONN_CB_ERROR, bbal_tcp_error);
+
+	cconn_data_set(prx->prx_back, prx);
+	cconn_on(prx->prx_back, CCONN_CB_CONNECTED, bbal_tcp_back_connect);
+	cconn_on(prx->prx_back, CCONN_CB_DATA_AVAILABLE, bbal_tcp_back_data);
+	cconn_on(prx->prx_back, CCONN_CB_END, bbal_tcp_back_end);
+	cconn_on(prx->prx_back, CCONN_CB_ERROR, bbal_tcp_error);
+
+	return;
+
+fail:
+	bbal_tcp_teardown(prx);
+	free(prx);
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -565,9 +835,10 @@ main(int argc, char *argv[])
 	cloop_t *loop = NULL;
 	cloop_ent_t *timer = NULL;
 	cloop_ent_t *udp = NULL;
+	cserver_t *tcp = NULL;
 
 	if (cloop_alloc(&loop) != 0 || cloop_ent_alloc(&udp) != 0 ||
-	    cloop_ent_alloc(&timer) != 0) {
+	    cloop_ent_alloc(&timer) != 0 || cserver_alloc(&tcp) != 0) {
 		warn("cloop init");
 		goto fail;
 	}
@@ -583,6 +854,9 @@ main(int argc, char *argv[])
 	}
 	cloop_ent_on(timer, CLOOP_CB_TIMER, run_timer);
 
+	/*
+	 * Listen on the UDP DNS port.
+	 */
 	if (bbal_listen_udp("0.0.0.0", "10053", &g_sock) != 0) {
 		warn("bbal_listen_udp");
 		goto fail;
@@ -593,9 +867,19 @@ main(int argc, char *argv[])
 	cloop_ent_on(udp, CLOOP_CB_READ, bbal_udp_read);
 	cloop_ent_want(udp, CLOOP_CB_READ);
 
+	/*
+	 * Listen on the TCP DNS port.
+	 */
+	cserver_on(tcp, CSERVER_CB_INCOMING, bbal_tcp_incoming);
+	if (cserver_listen_tcp(tcp, loop, "0.0.0.0", "10053") != 0) {
+		err(1, "cserver_listen");
+	}
+
+	int loopc = 0;
 	for (;;) {
 		unsigned int again = 0;
 
+		fprintf(stderr, "--------- LOOP %d\n", ++loopc);
 		if (cloop_run(loop, &again) != 0) {
 			warn("cloop_run");
 			goto fail;
